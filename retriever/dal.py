45967e486604db4fb2ee4cf2306055ab684b5106
david horovitz <dovidho@gmail.com>
2025-08-27 15:44:58 +0300

    level1

--- retriever/dal.py
+++ retriever/dal.py
@@ -1,32 +1,51 @@
 from pymongo import MongoClient
 import config
-import asyncio
+import time
 
 class Dal:
    def __init__(self):
        self.USER = config.MONGO_USER
        self.PASS = config.MONGO_PASS
        self.uri = config.MONGO_URI
+
        self.mongodb_client = MongoClient(self.uri)
        self.database = self.mongodb_client[config.MONGO_DB]
        self.collection = config.MONGO_COLLECTION
 
-   async def get_collection(self, skip=0 ,limit=100):
+   def get_collection(self, skip=0 ,limit=100):
        collection = self.database[self.collection]
        collection = list(collection.find().sort( 'CreateDate', 1).skip(skip).limit(limit))
        return collection
 
+   def get_splited_data(self, collection):
+       result = {
+           "antisemitic": [],
+           "non_antisemitic": []
+       }
+       for doc in collection:
+           # doc_id = str(doc.get("_id"))
+           if doc["Antisemitic"] == 1:
+               result["antisemitic"].append(doc)
+           else:
+               result["non_antisemitic"].append(doc)
+
+       return result
 
 
-async def main():
-    d = Dal()
-    skip = 0
-    limit = 100
-    while True:
-        docs = await d.get_collection(skip, limit)
-        for doc in docs:
-            print(doc)
-        skip += limit
-        await asyncio.sleep(8)
+   def main(self):
+       d = Dal()
+       skip = 0
+       limit = 100
+       while True:
+           docs = d.get_collection(skip, limit)
+           for doc in docs:
+               print(doc)
+           skip += limit
+           time.sleep(5)
 
-asyncio.run(main())
+
+
+# a=Dal()
+# a.main()
+
+
--- retriever/publisher.py
+++ retriever/publisher.py
@@ -5,25 +5,18 @@ class Publisher:
     def __init__(self):
         # self.event1={"App":"Producer 1"}
         # self.event2={"App": "Producer 2"}
-        self.topic1="raw_tweets_antisemitic"
-        self.topic2="raw_tweets_not_antisemitic"
+        self.topic_anti= "raw_tweets_antisemitic"
+        self.topic_no_anti= "raw_tweets_not_antisemitic"
 
 
     def get_producer_config(self):
         producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                                  value_serializer=lambda x:
-                                 json.dumps(x).encode('utf-8'))
+                                 json.dumps(x,default=str).encode('utf-8'))
         print(producer.config)
         return producer
 
     def publish_message(self,producer,topic,message):
-        """
-        This function will publish message to the topic which is received as a parameter
-        :param producer: producer object to publish the message to Kafka servers
-        :param topic: The topic to which the message will be published
-        :param message: The event message
-        :return: None
-        """
         producer.send(topic, message)
 
 
